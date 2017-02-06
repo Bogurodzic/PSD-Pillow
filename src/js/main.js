a6ab537d2cdfedac9eeb0b1f7de8c32f90501cfb
Kamil Jarzab <jarzbkamil@gmail.com>
2017-02-06 12:30:05 +0100

    Added slider scripts and styling

--- src/js/main.js
+++ src/js/main.js
@@ -4,4 +4,17 @@ $(document).ready(function(){
 		$(".header-menu ul").stop().toggle("slide", { direction: "right" });
 	});
 
+
+//Slider 
+
+	  $('.bxslider').bxSlider({
+	  	  nextSelector: '#slider-next',
+		  prevSelector: '#slider-prev',
+		  nextText: "<img src='img/arrow-right.png'>",
+		  prevText: "<img src='img/arrow-left.png'>",
+		  pager: false
+	  });
+
+
+
 });
